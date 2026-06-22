07bdee55cdf7b753fb5b55b54b5f6093759e51f4
Rodrigo Santana <Rodrigo.saitaman@gmail.com>
2026-06-22 11:22:19 -0300

    create player and enemy, move set,

--- code/const.py
+++ code/const.py
@@ -1,3 +1,4 @@
+import pygame
 from pygame.examples.grid import WINDOW_WIDTH, WINDOW_HEIGHT
 
 #c
@@ -6,6 +7,7 @@ COLOR_WHITE = (255, 255, 255)
 
 
 #E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Fase_A_1':0,
     'Fase_A_2':1,
@@ -14,9 +16,10 @@ ENTITY_SPEED = {
     'Fase_A_5':10,
     'Fase_A_6':5,
     'Fase_A_7':6,
-
-
-
+    'Player1': 4,
+    'Player2': 4,
+    'Enemy1': 2,
+    'Enemy2': 1,
 }
 
 #M
@@ -26,6 +29,20 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+#P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                'Player2': pygame.K_LCTRL}
+
+#S
+SPAWN_TIME = 4000
 
 #w
 #ESTABELECE DEFINIÇÕES DO TAMANHO DA JANELA (MESMA PROPORÇÃO DA IMAGEM DO MENU INICIAL.
--- code/entityFactory.py
+++ code/entityFactory.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
+
 from code.background import Background
-from code.const import WINDOW_WIDTH
+from code.const import WINDOW_WIDTH, WINDOW_HEIGHT
+from code.enemy import Enemy
+from code.player import Player
 
 
 class EntityFactory:
@@ -15,5 +19,13 @@ class EntityFactory:
                     list_bg.append(Background(f'Fase_A_{i}', (0, 0)))
                     list_bg.append(Background(f'Fase_A_{i}', (WINDOW_WIDTH, 0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1',(10, WINDOW_HEIGHT / 2 -30))
+            case 'Player2':
+                return Player('Player2', (10, WINDOW_HEIGHT / 2 + 30))
+            case 'Enemy1':
+                return Enemy('Enemy1',(WINDOW_WIDTH + 10,random.randint(30,WINDOW_HEIGHT - 30)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WINDOW_WIDTH + 10, random.randint(30, WINDOW_HEIGHT - 30)))
 
 
